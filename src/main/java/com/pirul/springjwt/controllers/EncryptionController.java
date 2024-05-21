4c35e32930a704e401784b1b340d1265b9cc281d
sanjay <sanjay.katheth@divihn.com>
2024-05-21 16:03:41 +0530

    file change

--- src/main/java/com/pirul/springjwt/controllers/EncryptionController.java
+++ src/main/java/com/pirul/springjwt/controllers/EncryptionController.java
@@ -28,41 +28,40 @@ import java.util.Base64;
 @RequestMapping("/encrypt")
 public class EncryptionController {
 
-    private static final String SECRET_KEY = "secret123";
-    
-    @Value("${encryption.key}")
-    private String key;
 
-    @Value("${encryption.initVector}")
-    private String initVector;
-    
-    @PostMapping("/credentials")
-    public ResponseEntity<?> encryptCredentials(@RequestBody EncryptionRequest encryptionRequest) {
-        String encryptedUsername = null;
-        String encryptedPassword = null;
+	@Value("${encryption.key}")
+	private String key;
 
-        try {
-            byte[] initVector = new byte[16];
-            new SecureRandom().nextBytes(initVector); // Generate a random IV
-            IvParameterSpec iv = new IvParameterSpec(initVector);
-            SecretKeySpec skeySpec = new SecretKeySpec(MessageDigest.getInstance("SHA-256").digest(key.getBytes("UTF-8")), "AES");
+	@Value("${encryption.initVector}")
+	private String initVector;
 
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
-            encryptedUsername = Base64.getEncoder().encodeToString(cipher.doFinal(encryptionRequest.getUsername().getBytes()));
-            encryptedPassword = Base64.getEncoder().encodeToString(cipher.doFinal(encryptionRequest.getPassword().getBytes()));
+	@PostMapping("/credentials")
+	public ResponseEntity<?> encryptCredentials(@RequestBody EncryptionRequest encryptionRequest) {
+		String encryptedUsername = null;
+		String encryptedPassword = null;
 
-            // Include the IV in the EncryptionResponse
-            EncryptionResponse encryptionResponse = new EncryptionResponse(
-                Base64.getEncoder().encodeToString(initVector),
-                encryptedUsername,
-                encryptedPassword
-            );
+		try {
+			byte[] initVector = new byte[16];
+			new SecureRandom().nextBytes(initVector); // Generate a random IV
+			IvParameterSpec iv = new IvParameterSpec(initVector);
+			SecretKeySpec skeySpec = new SecretKeySpec(
+					MessageDigest.getInstance("SHA-256").digest(key.getBytes("UTF-8")), "AES");
 
-            return ResponseEntity.ok(encryptionResponse);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error encrypting credentials");
-        }
-    }
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+			encryptedUsername = Base64.getEncoder()
+					.encodeToString(cipher.doFinal(encryptionRequest.getUsername().getBytes()));
+			encryptedPassword = Base64.getEncoder()
+					.encodeToString(cipher.doFinal(encryptionRequest.getPassword().getBytes()));
+
+			// Include the IV in the EncryptionResponse
+			EncryptionResponse encryptionResponse = new EncryptionResponse(
+					Base64.getEncoder().encodeToString(initVector), encryptedUsername, encryptedPassword);
+
+			return ResponseEntity.ok(encryptionResponse);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error encrypting credentials");
+		}
+	}
 }
