0b5493fbf4f5e0613ee6e1b03c9f3129316c8dbd
sanjay <sanjay.katheth@divihn.com>
2024-05-29 15:20:50 +0530

    added in global controller

--- src/main/java/com/pirul/springjwt/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pirul/springjwt/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -38,5 +39,13 @@ public class GlobalExceptionHandler {
 			WebRequest request) {
 		ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
 		return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+			HttpRequestMethodNotSupportedException ex, WebRequest request) {
+		String errorMessage = "Request method '" + ex.getMethod() + "' is not supported for this endpoint.";
+		ErrorResponse errorResponse = new ErrorResponse(errorMessage);
+		return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
 	}
 }
